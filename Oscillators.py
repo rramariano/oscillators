64de5220a0695e6d30b72c2ea4d440b1950e7912
Ross <rossm830@gmail.com>
2018-09-16 13:41:48 +0800

    Changed everything to class Pendulum

--- Oscillators.py
+++ Oscillators.py
@@ -1,136 +1,82 @@
 """
 Simulation of different kinds of oscillations
 """
-
 import numpy as np
+from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-# Different kind of oscillators as functions
-def simpleHarmonic(t,theta,omega):
-    """
-    with small angle approximation
-    """
+class Pendulum:
+
+    #constant
     g = 9.8
-    l = g
-    F = -(g*theta)/l
-    return F
 
-def anharmonic(t,x,v):
-    k = 1
-    alpha = 3
-    F = -k*(x**alpha)
-    return F
+    def __init__(self,dt=0.04,l=1,x=0.1,v=0.1,theta=0.2,omega=0):
+        self.time = np.arange(0,60,dt)
+        self.length = l
+        self.initX = x
+        self.initV = v
+        self.initTheta = theta
+        self.initOmega = omega
 
-def damped(t,theta,omega):
-    q = 1/2
-    F = simpleHarmonic(t,theta,omega) - q*omega
-    return F
+    #methods
+    #different kinds of pendulum/oscillatory motion
+    def simpleHarmonic(self,y,t):
+        """
+        Small angle approximation
+        F = -(g*theta)/l
+        theta(t0)=0.2
+        omega(t0)=0
+        """
+        theta, omega = y
+        dydt = [omega, -Pendulum.g*theta/self.length]
+        return dydt
 
-def dampedDriven(t,theta,omega):
-    fd = 1.2
-    od = 2/3
-    F = damped(t,theta,omega) + fd*np.sin(od*t)
-    return F
+    def anharmonic(self,y,t,k,alpha):
+        """
+        F = -k*(x**alpha)
+        """
+        x,v = y
+        dydt = [v,-k*(x**alpha)]
+        return dydt
 
-def chaoticNonlinear(t,theta,omega):
-    F = dampedDriven(t,np.sin(theta),omega)
-    return F
+    def damped(self,y,t,q=1/2):
+        """
+        F = -(g*theta)/ - q*omega
+        """
+        theta, omega = y
+        dydt = [omega, self.simpleHarmonic(y,t)[1] - q*omega]
+        return dydt
 
-def duffing(t,x,v):
-    """
-    https://en.wikipedia.org/wiki/Duffing_equation
-    """
-    delta = 0.3
-    alpha = -1
-    beta = 1
-    gamma = 0.5
-    omega = 1.2
-    F = gamma*np.cos(omega*t) - delta*v - alpha*x - beta*x**3
-    return F
+    def dampedDriven(self,y,t,fd=1.2,od=2/3):
+        """
+        F = -(g*theta)/l - q*omega + fd*sin(od*t)
+        """
+        theta,omega = y
+        dydt = [omega, self.damped(y,t)[1] + fd*np.sin(od*t)]
+        return dydt
 
-def rayleighLorentz(t,x,v):
-    """
-    https://en.wikipedia.org/wiki/Rayleigh%E2%80%93Lorentz_pendulum
-    """
-    omega = t/100
-    F = -(omega**2)*x
-    return F
+    def chaoticNonLinear(self,y,t):
+        """
+        F = -(g*sin(theta))/l - q*omega + fd*sin(od*t)
+        """
+        theta, omega = y
+        theta = np.sin(y[0])
+        z=(theta,omega)
+        dydt = [omega, self.dampedDriven(z,t)[1]]
+        return dydt
 
-# Simulation
-def swing(dt):
-    # initialize time
-    t = np.arange(0,60,dt)
+    def duffing(self,y,t,delta=0.3,alpha=-1,beta=1,gamma=0.5,omega=1.2):
+        """
+        https://en.wikipedia.org/wiki/Duffing_equation
+        """
+        x, v = y
+        dydt = [v, gamma*np.cos(omega*t) - delta*v - alpha*x - beta**3]
+        return dydt
 
-    initVelocity = 0
-    initPosition = 0.2
-    
-    # function to observe
-    pendulum = rayleighLorentz
-
-    # initialize array
-    positions = np.zeros(len(t))
-    velocities = np.zeros(len(t))
-
-    # initial conditions
-    velocities[0] = initVelocity
-    positions[0] = initPosition
-
-
-    # Euler method (first order differential)
-    # Euler-Cromer method (second order differential)
-    # Approximates next value using Euler/Euler-Cromer method
-    # y[n+1] = y[n] + h*f(t[n],y[n])
-    for i in range(len(t)-1):
-        velocities[i+1] = velocities[i] + dt*pendulum(t[i],positions[i],velocities[i])
-        positions[i+1] = positions[i] + dt*velocities[i+1]
-    # plt.plot(t,positions)
-
-    # Reinitialize
-    positions = np.zeros(len(t))
-    velocities = np.zeros(len(t))
-    velocities[0] = initVelocity
-    positions[0] = initPosition
-
-    # Midpoint method
-    # y[n+1] = y[n] + h*f(t[n]+h/2,y[n]+(h/2)*f(t[n],y[n]))
-    for i in range(len(t)-1):
-        velocities[i+1] = velocities[i] + dt*pendulum(t[i]+dt/2,
-                                                            positions[i]+(dt/2)*pendulum(t[i],positions[i],velocities[i]),
-                                                            velocities[i]+(dt/2)*pendulum(t[i],positions[i],velocities[i]))
-        positions[i+1] = positions[i] + dt*velocities[i+1]
-    # plt.plot(t,positions)
-
-    # Reinitialize
-    positions = np.zeros(len(t))
-    velocities = np.zeros(len(t))
-    velocities[0] = initVelocity
-    positions[0] = initPosition
-
-    # Runge-Kutta method (RK4)
-    # y[n+1] = y[n] + 1/6(k1+2k2+2k3+k4)
-    # k1 = h*f(t[n],y[n])
-    # k2 = h*f(t[n]+h/2,y[n]+h/2)
-    # k3 = h*f(t[n]+h/2,y[n]+k2/2)
-    # k4 = h*f(t[n]+h,y[n]+k3)
-    for i in range(len(t)-1):
-        k1 = dt*pendulum(t[i],positions[i],velocities[i])
-        k2 = dt*pendulum(t[i]+dt/2,positions[i]+k1/2,velocities[i]+k1/2)
-        k3 = dt*pendulum(t[i]+dt/2,positions[i]+k2/2,velocities[i]+k2/2)
-        k4 = dt*pendulum(t[i]+dt,positions[i]+k3,velocities[i]+k3)
-        velocities[i+1] = velocities[i] + (1/6)*(k1+2*k2+2*k3+k4)
-        positions[i+1] = positions[i] + dt*velocities[i+1]
-    plt.plot(t,positions)
-
-    plt.show()
-
-    return None
-
-def main():
-    # Lower time step value gives a more accurate solution
-    # but also increases memory usage and running time
-    TIME_STEP = 0.1
-    swing(TIME_STEP)
-    return None
-
-if __name__ == "__main__":
-    main()
+    def rayleighLorentz(self,y,t,omega=1.2):
+        """
+        https://en.wikipedia.org/wiki/Rayleigh%E2%80%93Lorentz_pendulum
+        """
+        x, v = y
+        dydt = [v, (omega**2)*x]
+        return dydt
